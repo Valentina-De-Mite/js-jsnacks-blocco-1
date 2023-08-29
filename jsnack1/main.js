1e73ebae278ee168da77d067cf4d5ab29a3da960
Valentina De Mite <valesakura97@gmail.com>
2023-08-29 14:53:55 +0200

    complete snack 1

---
+++ jsnack1/main.js
@@ -0,0 +1,45 @@
+/*
+Snack 1:
+L’utente inserisce due numeri in successione, con due prompt.
+Il software stampa il maggiore.
+*/
+
+// TOOLS:
+// - prompt x2
+// - if else
+// - inner.HTML
+
+// chiedo all'utente di inserire due numeri
+
+let numbers = document.getElementById("numbers");
+
+var message = "";
+
+let userNumber1 = prompt("Inserisci qui il primo numero");
+console.log(userNumber1);
+
+let userNumber2 = prompt("Inserisci qui il secondo numero");
+console.log(userNumber2);
+
+// confronto i due numeri
+
+if (userNumber1 > userNumber2) {
+  message = "Il numero piu' alto e' ";
+}
+if (userNumber1 < userNumber2) {
+  message = "Il numero piu' alto e' ";
+}
+if (userNumber1 === userNumber2) {
+  message = "I numeri sono uguali: ";
+} else if ("") {
+  message = "insewrisci nuovamente 2 numeri ";
+}
+
+var numberList = [userNumber1, userNumber2];
+console.log(message + Math.max(userNumber1, userNumber2));
+
+number = Math.max(userNumber1, userNumber2);
+
+// stampo in pagina il maggiore
+
+numbers.innerHTML = message + number;
